b9d36dbd0b0e42d4419c0268a626d4196afa8278
Dynamicdra90n <aidenvanlandingham@gmail.com>
2026-02-18 13:43:31 -0500

    Update Hello.java
    changes to syntax and added "!" to the hello message

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-pulic class Hello{
+pulic class Hello {
   public static void main (String args[]) {
-    System.out.println("Hello from GitHub Actions");
+    System.out.println("Hello from GitHub Actions!");
   }
 }
