99d7aba2ea92dd4a26451550bb036d5bf0b29d96
Dynamicdra90n <aidenvanlandingham@gmail.com>
2026-02-18 14:09:30 -0500

    Update Hello.java to fail

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello{
-  public static void main (String args[]){
+public class Hell0 {
+  public static void main (String args[]) {
     System.out.println("Hello from GitHub Actions!");
   }
 }
